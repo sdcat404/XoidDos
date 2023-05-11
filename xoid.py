28ed2e865fdb6168126555986358f17839e80eb3
Scotty <118654680+sdcat404@users.noreply.github.com>
2023-05-11 11:41:42 +0100

    Update xoid.py

--- xoid.py
+++ xoid.py
@@ -627,7 +627,7 @@ def main():
             except EnvironmentError:
                 error("cannot read file {0}".format(uas_file))
 
-        xoid = xoid(url)
+        xoid = Goldeneye(url)
         xoid.useragents = useragents
         xoid.nr_workers = workers
         xoid.method = method
